4a59f627e495a1775298eec5b2a1c55379d6de3f
SquirrelBrain <jkeith@nimdapro.com>
2020-10-06 20:57:33 -0500

    Laying the project foundation

---
+++ manage.py
@@ -0,0 +1,31 @@
+"""
+Django's Command-Line Utility for executing Administrative Tasks
+Docs: https://docs.djangoproject.com/en/dev/ref/django-admin/
+"""
+
+import os
+import sys
+
+
+def execute_subcommand():
+    """
+    Execute Administrative Subcommand
+    Usage: python3 manage.py <sub_command> <options>
+    """
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          '{{ project_name }}.settings.development')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            'Failed to import Django Web Framework. Did you activate your '
+            'Virtual Environment? Are you sure that Django is installed '
+            'and available on your PYTHONPATH?'
+        ) from exc
+    execute_from_command_line
+
+
+if __name__ == '__main__':
+    execute_subscommand()
---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+/*
+ * {{ project_name }} v1.0.0
+ * Copyright 2020
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ */

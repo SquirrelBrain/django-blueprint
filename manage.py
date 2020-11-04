10d081bd03dae3645c38143b7201dd443c38ca51
SquirrelBrain <jkeith@nimdapro.com>
2020-11-04 15:00:32 -0600

    Removed the unnecessary function

--- manage.py
+++ manage.py
@@ -1,31 +1,23 @@
-"""
-Django's Command-Line Utility for executing Administrative Tasks
-Docs: https://docs.djangoproject.com/en/dev/ref/django-admin/
-"""
+# Django's Command-Line Utility for executing Administrative Tasks
+# Docs: https://docs.djangoproject.com/en/{{ docs_version }}/ref/django-admin/
 
 import os
 import sys
 
 
-def execute_subcommand():
-    """
-    Execute Administrative Subcommand
-    Usage: python3 manage.py <sub_command> <options>
-    """
+if __name__ == '__main__':
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          '{{ project_name }}.settings.development')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        '{{ project_name }}.settings.development'
+    )
 
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as Exc:
         raise ImportError(
-            'Failed to import Django Web Framework. Did you activate your '
-            'Virtual Environment? Are you sure that Django is installed '
-            'and available on your PYTHONPATH?'
-        ) from exc
+            'Failed to import Django Web Framework. Did you activate '
+            'your Virtual Environment? Is Django installed and available '
+            'on your PYTHONPATH?'
+        ) from Exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    execute_subcommand()
